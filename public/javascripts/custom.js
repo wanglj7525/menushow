56963830d62bb194effb0c621c48b286e9376ef2
wanglj7525 <wanglj7525@163.com>
2015-11-04 21:27:03 +0800

    图表测试

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,31 +1,80 @@
-		var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 		var lineChartData = {
 			labels : ["January","February","March","April","May","June","July"],
 			datasets : [
 				{
-					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
+					fillColor : "rgba(220,220,220,0.5)",
 					strokeColor : "rgba(220,220,220,1)",
 					pointColor : "rgba(220,220,220,1)",
 					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+					data : [65,59,90,81,56,55,40]
 				},
 				{
-					label: "My Second dataset",
-					fillColor : "rgba(151,187,205,0.2)",
+					fillColor : "rgba(151,187,205,0.5)",
 					strokeColor : "rgba(151,187,205,1)",
 					pointColor : "rgba(151,187,205,1)",
 					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(151,187,205,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+					data : [28,48,40,19,96,27,100]
 				}
 			]
-
-		}
-
+		};
+		function lineChart() {
+	        var ctx = document.getElementById('sales-volume-chart').getContext("2d")
+	        var data = {
+	            labels: ["2014-10", "2014-11", "2014-12", "2015-1", "2015-2", "2015-3"],
+	            datasets: [{
+	                label: "",
+	                fillColor: "rgba(220,220,220,0.2)",
+	                strokeColor: "rgba(0,102,51,1)",
+	                pointColor: "rgba(220,220,220,1)",
+	                pointStrokeColor: "#339933",
+	                pointHighlightFill: "#339933",
+	                pointHighlightStroke: "rgba(220,220,220,1)",
+	                data: [1.27, 1.30, 1.30, 1.41, 1.04, 1.29]
+	            }]
+	        };
+	        // var salesVolumeChart = new Chart(ctx).Line(data);
+	        var salesVolumeChart = new Chart(ctx).Line(data, {
+	            // 小提示的圆角
+	            // tooltipCornerRadius: 0,
+	            // 折线的曲线过渡，0是直线，默认0.4是曲线
+	            bezierCurveTension: 0,
+	            // bezierCurveTension: 0.4,
+	            // 关闭曲线功能
+	            bezierCurve: false,
+	            // 背景表格显示
+	            // scaleShowGridLines : false,
+	            // 点击的小提示
+	            tooltipTemplate: "<%if (label){%><%=label%> 销量：<%}%><%= value %>万辆",
+	            //自定义背景小方格、y轴每个格子的单位、起始坐标
+	            scaleOverride: true,
+	            scaleSteps: 9.5,
+	            // scaleStepWidth: Math.ceil(Math.max.apply(null,data.datasets[0].data) / 0.1),
+	            scaleStepWidth: 0.05,
+	            scaleStartValue: 1
+	        });
+	    }
+	    function barChart() {
+	        var ctx = document.getElementById('sales-volume-bar-chart').getContext("2d")
+	        var data = {
+	            labels: ["2014-10", "2014-11", "2014-12", "2015-1", "2015-2", "2015-3"],
+	            datasets: [{
+	                label: "",
+	                fillColor: "rgba(153,204,153,0.5)",
+	                strokeColor: "rgba(0,102,51,1)",
+	                pointColor: "rgba(220,220,220,1)",
+	                pointStrokeColor: "#338033",
+	                pointHighlightFill: "#338033",
+	                pointHighlightStroke: "rgba(220,220,220,1)",
+	                data: [1.27, 1.30, 1.30, 1.41, 1.04, 1.29]
+	            }]
+	        };
+	        var salesVolumeChart = new Chart(ctx).Bar(data, {
+	            // 点击的小提示
+	            tooltipTemplate: "<%if (label){%><%=label%> 销量：<%}%><%= value %>万辆"
+	        });
+	    }
+		var graphInitDelay = 300;
+		var globalGraphSettings = {animation : Modernizr.canvas};
 $(function() {
 	$(document).on('blur','input',function() {
 		if ($(this).closest('div').hasClass('errorshow')
@@ -33,7 +82,10 @@ $(function() {
 			$(this).closest('div').removeClass('errorshow');
 		}
 	});
+	
+
 });
+
 function login(){
 
 	var username=$("#username").val();
